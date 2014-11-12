71678dcf4832db793ef0c79a715e10d01e823fbc
Timo Kaipiainen <timo.kaipiainen@gmail.com>
2014-11-12 23:42:48 +0200

    preliminary test baseline

---
+++ src/JYPS/RegisterBundle/Tests/Controller/MemberControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JYPS\RegisterBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Endroid\QrCode\QrCode;
+
+class MemberControllerTest extends WebTestCase
+{
+	public function testSimpleJoin() {
+ 		$client = static::createClient();
+
+        $crawler = $client->request('GET', '/join');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Henkilötiedot")')->count()
+        );	
+
+        $buttonCrawlerNode = $crawler->selectButton('memberid_save');
+        $form = $buttonCrawlerNode->form();
+
+        $form = $buttonCrawlerNode->form(array(
+    	'memberid[firstname]' => 'Teuvo',
+    	'memberid[surname]'   => 'Testaaja',
+		));
+       # $client->submit($form);
+    }
+}
